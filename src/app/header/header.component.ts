4de112727c1889278ae85e86bd147ec19a4d7d29
idea-PC\g480-866181 <aquibmomin007@gmail.com>
2018-04-08 18:27:37 +0800

    initial commit

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+
+@Component({ 
+  selector: 'app-header',
+  templateUrl: './header.component.html'
+})
+export class HeaderComponent {
+	@Output() loadFeature = new EventEmitter<string>();
+
+	onSwitchTab(tabType:string){
+		this.loadFeature.emit(tabType);
+	}
+
+}
